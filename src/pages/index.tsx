7e47a790ed8d8c81bfc16069042840eebc23d687
DevSP <contatodevsp@gmail.com>
2021-03-28 22:53:24 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { stripe } from '../services/stripe'
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Ia9a2KzfJRoYNh9X4fzvZxz', {
     expand: ['product']
   })
@@ -55,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 // seg, hour, days -> 24h
   }
 }
